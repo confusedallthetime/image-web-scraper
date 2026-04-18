3f53602c89564bfd8b8912899629ee7caf676405
my biggest fear <chendy@oreongstate,.edu>
2026-04-17 23:51:21 -0700

    updated to match images with name

--- scrape.js
+++ scrape.js
@@ -9,40 +9,43 @@ puppeteer.use(StealthPlugin());
 
   const browser = await puppeteer.launch({
     headless: false,
-    targetFilter: target => !!target.url(),
   });
 
   var page = await browser.pages();
   page = page[0];
 
-  await page.goto("https://csgoskins.gg/containers/paris-2023-contenders-sticker-capsule");
+  await page.goto("https://csgoskins.gg/categories/weapon-case");
 
   //console.log(await page.content());
 
-  const cdnLinks = await page.$$eval( ".left-4.right-4.absolute.group.top-\\[140px\\].lazy-hover", 
-    links => links.map(link => link.getAttribute("data-lazy-hover-src")))
+  const parentItems = await page.$$eval(".mx-auto.max-h-\\[237px\\]", 
+    pitems => pitems.map(pitem => {
+      return {
+        url: pitem?.getAttribute("src") || null,
+        name: pitem?.getAttribute("alt") || null
+      }
+    }
+    ));
 
-
-  const stickerNames = await page.$$eval(".mx-auto.max-h-\\[237px\\]", names => names.map(name => name.getAttribute("alt")))
+  for (let i = 0; i < parentItems.length; i++){
+    const url = parentItems[i].url;
+    const name = parentItems[i].name;
   
-  for (let i = 0; i < cdnLinks.length; i++){
-    const url = cdnLinks[i];
-    const name = stickerNames[i];
-  const safeName = name.replace(/\|/g, ".");
-
+    const safeName = name.replace(/[|:]/g, ".");
     
+
     const response = await axios.get(url, {
       responseType: "arraybuffer",
       headers: {
         "User-Agent": "Mozilla/5.0",
-        "Accept": "image/avif,image/webp,image/*,*/*"
+        "Accept": "image/avif,image/webp,image/,/*"
       }
     });
 
     const data = Buffer.from(response.data);
 
-    const dirPath = "./server/data/images/paris2023/contender-sticker";
-    const filePath = dirPath + `/${safeName}.webp`;
+    const dirPath = "../csmultisell/public/weaponcase/";
+    const filePath = dirPath + safeName + ".webp";
 
     if (!fs.existsSync(dirPath)) {
       fs.mkdirSync(dirPath, { recursive: true });
@@ -56,4 +59,4 @@ puppeteer.use(StealthPlugin());
 
   await page.close()
   await browser.close()
-})(); 
+})();
